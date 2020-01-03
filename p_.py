cfbdb0706f636f96111430a18b2a7586314765ee
Jonathan Matos <54323968+gianelaudia@users.noreply.github.com>
2020-01-03 11:38:10 -0500

    Add files via upload

---
+++ p_.py
@@ -0,0 +1,15 @@
+print ("     ")
+print ("   ¿ Quieres saber que tan viejo estás?   ")
+print ("      ")
+
+v=18
+p=5
+a=2019
+c=int(input("Bueno, si naciste en "))
+s=(a-c)
+
+if s>=v: print("entonces tienes " + str(s) + " años, asi que eres un anciano")
+
+if s<=v: print("entonces tienes " + str(s) + " años, estas chikito")
+
+if s<p: print("wtf, que haces aqui?")
